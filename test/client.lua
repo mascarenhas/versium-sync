4eb47dd12641cdaf9415e487daea21aaf2f3cac9
Fabio Mascarenhas <mascarenhas@minilambda.local>
2009-04-21 00:09:10 -0300

    more test cases for client

--- test/client.lua
+++ test/client.lua
@@ -124,6 +124,110 @@ function test_update_changes()
   stop_server()
 end
 
+function test_update_conflict()
+  local srepo = make_server_repo()
+  local crepo = empty_repo()
+  start_server()
+  local c = versium.sync.client.new(crepo, "http://localhost:8080/server.ws", blacklist)
+  local confs, changes = c:update()
+  assert(empty(confs))
+  changes = set2pairs(changes)
+  assert(#changes == 4)
+  for _, node in ipairs(changes) do
+    assert(srepo:get_node(node[1]) == crepo:get_node(node[1], node[2]))
+  end
+  srepo:save_version("Bar", "new version of bar", "mascarenhas")
+  srepo:save_version("Page", "a new page", "carregal")
+  crepo:save_version("Bar", "conflict on bar", "carregal")
+  local confs, changes = c:update()
+  assert(#confs == 1)
+  assert(crepo:get_node("Bar") == "conflict on bar")
+  for _, conf in ipairs(confs) do
+    assert(conf.text == srepo:get_node(conf.id, conf.info.version))
+    crepo:save_version(conf.id, conf.text, conf.info.author)
+  end
+  changes = set2pairs(changes)
+  assert(#changes == 1)
+  for _, node in ipairs(changes) do
+    assert(srepo:get_node(node[1]) == crepo:get_node(node[1], node[2]))
+  end
+  local confs, changes = c:update()
+  assert(empty(confs))
+  assert(empty(changes))
+  stop_server()
+end
+
+function test_commit()
+  local srepo = make_server_repo()
+  local crepo = empty_repo()
+  start_server()
+  local c = versium.sync.client.new(crepo, "http://localhost:8080/server.ws", blacklist)
+  local confs, changes = c:update()
+  assert(empty(confs))
+  changes = set2pairs(changes)
+  assert(#changes == 4)
+  for _, node in ipairs(changes) do
+    assert(srepo:get_node(node[1]) == crepo:get_node(node[1], node[2]))
+  end
+  local new_nodes = {
+    { "Bar", "new version of bar", "mascarenhas" },
+    { "Page", "a new page", "carregal" }
+  }
+  for _, node in ipairs(new_nodes) do
+    crepo:save_version(unpack(node))
+  end
+  local confs = c:commit()
+  assert(#confs == 0)
+  for _, node in ipairs(new_nodes) do
+    assert(srepo:get_node(node[1]) == node[2])
+    assert(srepo:get_node_info(node[1]).author == node[3])
+  end
+  local confs, changes = c:update()
+  assert(empty(confs))
+  assert(empty(changes))
+  stop_server()
+end
+
+function test_commit_conflict()
+  local srepo = make_server_repo()
+  local crepo = empty_repo()
+  start_server()
+  local c = versium.sync.client.new(crepo, "http://localhost:8080/server.ws", blacklist)
+  local confs, changes = c:update()
+  assert(empty(confs))
+  changes = set2pairs(changes)
+  assert(#changes == 4)
+  for _, node in ipairs(changes) do
+    assert(srepo:get_node(node[1]) == crepo:get_node(node[1], node[2]))
+  end
+  local new_nodes = {
+    { "Bar", "new version of bar", "mascarenhas" },
+    { "Page", "a new page", "carregal" },
+    { "Foo", "new version of foo", "mascarenhas" }
+  }
+  for _, node in ipairs(new_nodes) do
+    crepo:save_version(unpack(node))
+  end
+  srepo:save_version(unpack(new_nodes[1]))
+  srepo:save_version(unpack(new_nodes[2]))
+  local confs = c:commit()
+  assert(#confs == 2)
+  for i = 3, #new_nodes do
+    local node = new_nodes[1]
+    assert(srepo:get_node(node[1]) == node[2])
+    assert(srepo:get_node_info(node[1]).author == node[3])
+  end
+  local confs, changes = c:update()
+  assert(empty(confs))
+  assert(not empty(changes))
+  local confs = c:commit()
+  assert(#confs == 0)
+  local confs, changes = c:update()
+  assert(empty(confs))
+  assert(empty(changes))
+  stop_server()
+end
+
 for n, f in pairs(_G) do
   if type(n) == "string" and n:match("^test_") then
     pcall(f)
